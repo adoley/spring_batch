87bfbd5e29c7f545e588e6aa0a9b92543583d176
Apollo Doley <apollo.doley@aexp.com>
2019-04-27 15:02:02 +0530

    refactored the class name

--- src/main/java/jobs/HelloWorldTaskLet.java
+++ src/main/java/jobs/HelloWorldTaskLet.java
@@ -5,7 +5,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-public class MyTaskLet implements Tasklet {
+public class HelloWorldTaskLet implements Tasklet {
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
         System.out.println("My first spring batch job");
         return RepeatStatus.FINISHED;
