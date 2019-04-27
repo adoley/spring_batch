7e8cde31e9a1d0f594bddf3f84f1a7241d3db465
Apollo Doley <apollo.doley@aexp.com>
2019-04-27 14:54:59 +0530

    spring batch hello world

---
+++ src/main/java/jobs/MyTaskLet.java
@@ -0,0 +1,13 @@
+package jobs;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class MyTaskLet implements Tasklet {
+    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("My first spring batch job");
+        return RepeatStatus.FINISHED;
+    }
+}
